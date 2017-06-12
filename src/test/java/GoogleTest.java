190de10a1449870cd295039aa9c6f8f6ab03ce55
AnnaMameko <anna.vilyams@gmail.com>
2017-06-12 22:45:41 +0300

    Starting with selenium testing

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,49 @@
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+public class GoogleTest {
+
+    WebDriver driver;
+
+    @Before
+    public void start(){
+        ChromeDriverManager.getInstance().setup();
+        /*FirefoxDriverManager.getInstance().setup();
+        OperaDriverManager.getInstance().setup();
+        PhantomJsDriverManager.getInstance().setup();
+        EdgeDriverManager.getInstance().setup();
+        InternetExplorerDriverManager.getInstance().arch32().setup();*/
+
+        driver = new ChromeDriver();
+        /* driver = new FirefoxDriver();
+        driver = new OperaDriver();
+        driver = new PhantomJSDriver();
+        driver = new EdgeDriver();
+        driver = new InternetExplorerDriver(); */
+    }
+
+    @Test
+    public void google_test() throws InterruptedException {
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        Thread.sleep(3000);
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+    }
+}
