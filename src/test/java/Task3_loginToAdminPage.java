8a0d1a418465861d7993cefe5223560fbf754bdf
AnnaMameko <anna.vilyams@gmail.com>
2017-06-23 17:47:26 +0300

    Task 3: Create simple scenario which performs login to Admin page

---
+++ src/test/java/Task3_loginToAdminPage.java
@@ -0,0 +1,46 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by anna.mameko on 6/23/2017.
+ */
+public class Task3_loginToAdminPage {
+    WebDriver driver;
+
+    @Before
+    public void start(){
+        ChromeDriverManager.getInstance().setup();
+        /*FirefoxDriverManager.getInstance().setup();
+        OperaDriverManager.getInstance().setup();
+        PhantomJsDriverManager.getInstance().setup();
+        EdgeDriverManager.getInstance().setup();
+        InternetExplorerDriverManager.getInstance().arch32().setup();*/
+
+        driver = new ChromeDriver();
+        /* driver = new FirefoxDriver();
+        driver = new OperaDriver();
+        driver = new PhantomJSDriver();
+        driver = new EdgeDriver();
+        driver = new InternetExplorerDriver(); */
+    }
+
+    @Test
+    public void login() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).submit();
+        //Thread.sleep(5000);
+
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+    }
+}
